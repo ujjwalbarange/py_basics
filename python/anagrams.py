6c1523d46408191617374467bb515fcecf6b6027
Ujjwal Barange <145079274+ujjwalbarange@users.noreply.github.com>
2025-08-14 14:51:24 +0000

    update

--- python/anagrams.py
+++ python/anagrams.py
@@ -2,3 +2,9 @@ a=list(input("1st word"))
 b=list(input("2nd word"))
 print(a)
 print(b)
+for i in a:
+    if i in b:
+        b.remove(i)
+    else:
+        print("Not an anagram")
+        break
--- python/pandas/read.py
+++ python/pandas/read.py
@@ -1,5 +1,5 @@
 import pandas
 df1=pandas.read_csv("csvf.csv", header=None)
 df1.columns = ["name","roll"]
-df1.set_index("roll")
+df1=df1.set_index("name")
 print(df1)
